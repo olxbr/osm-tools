0d800e3968349b8b4b1be71d7cd7643175afb12f
Alan Anselmo <alan.anselmo@olxbr.com>
2023-01-10 21:38:18 -0300

    private_key_finder.sh update; dynamodb table to audit keypair scans

--- ec2/osm-ec2-check-keypair-lambda/lambda_function.py
+++ ec2/osm-ec2-check-keypair-lambda/lambda_function.py
@@ -3,14 +3,19 @@ import json
 import logging
 import boto3
 from boto3.dynamodb.conditions import Key
+from datetime import datetime
 
 log = logging.getLogger(__name__)
 KEYPAIRS_TABLE = os.getenv('KEYPAIRS_TABLE', 'osm_ec2_keypairs')
+KEYPAIRS_SUMMARY_TABLE = os.getenv('KEYPAIRS_TABLE', 'osm_ec2_keypairs_summary')
 
 
 def lambda_handler(event, context):
     body = json.loads(event.get('body', '{}'))
     fingerprint = body.get('fingerprint')
+    account = body.get('account')
+    instance = body.get('instance')
+    filename = body.get('filename')
 
     if fingerprint is None:
         return {
@@ -20,22 +25,41 @@ def lambda_handler(event, context):
             })
         }
 
-    dynamo_table = boto3.resource('dynamodb').Table(KEYPAIRS_TABLE)
+    keypairs_table = boto3.resource('dynamodb').Table(KEYPAIRS_TABLE)
+    summary_table = boto3.resource('dynamodb').Table(KEYPAIRS_SUMMARY_TABLE)
+    found_fingerprint = ''
 
     try:
-        result = dynamo_table.query(
-            IndexName="fingerprint-index",
-            KeyConditionExpression=Key('fingerprint').eq(fingerprint),
-        )
-        if result['Count'] > 0:
-            return {
-                'statusCode': 200,
-                'body': json.dumps({'exists': True})
-            }
+        for fp in fingerprint.values():
+            if fp != '':
+                result = keypairs_table.query(
+                    IndexName="fingerprint-index",
+                    KeyConditionExpression=Key('fingerprint').eq(fp)
+                )
+                if result['Count'] > 0:
+                    found_fingerprint = fp
+                    break
     except Exception as err:
         log.error(f'DynamoDB query error: {err}')
 
+    found = found_fingerprint != ''
+
+    try:
+        item = {
+            'account': account,
+            'instance': instance,
+            'filename': filename,
+            'scan_date': datetime.utcnow().astimezone().isoformat()
+        }
+
+        if found:
+            item['found_fingerprint'] = found_fingerprint
+
+        _ = summary_table.put_item(Item=item)
+    except Exception as err:
+        log.error(f'DynamoDB put_item error: {err}')
+
     return {
-        'statusCode': 404,
-        'body': json.dumps({'exists': False})
+        'statusCode': 200 if found else 404,
+        'body': json.dumps({'found_keypair': found})
     }
--- ec2/osm-keypair-scan/private_key_finder.sh
+++ ec2/osm-keypair-scan/private_key_finder.sh
@@ -3,15 +3,6 @@ filename="$1"
 auth="<auth-key>"
 api_url="<api-url>"
 
-cmd() {
-    fingerprint=$1
-    echo $(curl -s -w "%{http_code}" -o /dev/null \
-        -H "authorization: ${auth}" \
-        -H "content-type: application/json" \
-        -d "{\"fingerprint\": \"${fingerprint}\"}" \
-        ${api_url}/ec2-check-keypair)
-}
-
 if [[ "$filename" =~ python[0-9.]+[\/test|\/site-packages] ||
     "$filename" =~ ruby\/gems\/[0-9]+\.[0-9]+\.[0-9]+ ||
     "$filename" =~ node_modules ||
@@ -26,24 +17,36 @@ fi
 
 if command -v ssh-keygen &>/dev/null; then
     sha256=$(ssh-keygen -lf "$filename" 2>/dev/null | awk '{print $2}')
-    if [ $(cmd ${sha256/* }) -eq "200" ]; then
-        rm -f $filename
-        exit 0
-    fi
 fi
 
 if command -v openssl &>/dev/null; then
     sha1=$(openssl pkcs8 -in "$filename" -inform PEM -outform DER -topk8 -nocrypt 2>/dev/null| openssl sha1 -c)
-    if [ $(cmd ${sha1/* }) -eq "200" ]; then
-        rm -f $filename
-        exit 0
-    fi
-
     md5=$(openssl rsa -in "$filename" -pubout -outform DER 2>/dev/null | openssl md5 -c)
-    if [ $(cmd ${md5/* }) -eq "200" ]; then
-        rm -f $filename
-        exit 0
-    fi
+fi
+
+instance = $(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | grep '"instanceId"' | cut -d\" -f4)
+account = $(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | grep '"accountId"' | cut -d\" -f4)
+
+json_data='{
+   "filename": "'"$filename"'",
+   "fingerprint": {
+       "sha256": "'"${sha256/* }"'",
+       "sha1": "'"${sha1/* }"'",
+       "md5": "'"${md5/* }"'"
+    },
+    "instance": "'"$instance"'",
+    "account": "'"$account"'"
+}'
+
+status_code=$(curl -s -w "%{http_code}" -o /dev/null \
+    -H "authorization: $auth" \
+    -H "content-type: application/json" \
+    -d "$json_data" \
+    $api_url/ec2-check-keypair)
+
+if [ $status_code -eq "200" ]; then
+    rm -f $filename
+    exit 0
 fi
 
 exit 0
